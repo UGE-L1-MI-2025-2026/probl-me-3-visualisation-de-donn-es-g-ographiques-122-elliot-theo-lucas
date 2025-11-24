72a434051b47086e4f9d60b6696dc39b91c225ec
ELLIOT THOMAS <elliot.thomas@nomachine.univ-eiffel.fr>
2025-11-24 11:42:45 +0100

    opedfpiouzesfpo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from draw import Drawer
-# import pyshp
-# imports en commentaires vu qu'on va surement les importer dans d'autres fichiers et non dans le main
+from shapefile import Reader
 
 
 if __name__ == "__main__":
