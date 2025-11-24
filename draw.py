6a774d7a07a7d483e99043fbf0a069f2f2692580
ELLIOT THOMAS <elliot.thomas@nomachine.univ-eiffel.fr>
2025-11-24 12:42:23 +0100

    added structure and base functions

--- draw.py
+++ draw.py
@@ -1,7 +1,24 @@
 import fltk
+from typing import List, Tuple
 
 class Drawer:
 
     def __init__(self) -> None:
 
-        pass
+        self.window : Tuple[int, int] = 500, 300
+        fltk.cree_fenetre(self.window[0], self.window[1])
+    
+    @staticmethod
+    def draw_departement(points : List[Tuple[float, float]]) -> None:
+
+        fltk.polygone(points)
+
+    def update(self) -> bool:
+        event = fltk.attend_ev()
+        event_type = fltk.type_ev(event)
+
+        if event_type == "Quitte": return False 
+
+        return True
+
+    def exit(self) -> None: fltk.ferme_fenetre()
--- main.py
+++ main.py
@@ -2,4 +2,13 @@ from draw import Drawer
 from shapefile import Reader
 
 if __name__ == "__main__":
-    pass 
+    sf = Reader("fr_100km.dbf")
+    sf.records()
+
+    drawer = Drawer()
+    
+    while True:
+        drawer.draw_departement(sf.shape(75).points)
+        if not drawer.update(): break
+
+    drawer.exit()
