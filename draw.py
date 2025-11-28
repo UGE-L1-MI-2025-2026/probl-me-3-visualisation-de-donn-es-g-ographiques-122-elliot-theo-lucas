8e7005a82a1c3db0125677ba5434335100d50bfd
ELLIOT THOMAS <elliot.thomas@nomachine.univ-eiffel.fr>
2025-11-28 11:41:14 +0100

    restructuration v3

--- draw.py
+++ draw.py
@@ -1,6 +1,17 @@
 import fltk
 from typing import List, Tuple, Dict
 
+class Polygon:
+
+    def __init__(self, points : List[Tuple[int, int]], colour : str = "black", fill : str = "", thickness : float = 2.0) -> None:
+        self.points : List[Tuple[int, int]] = points
+        self.colour : str = colour
+        self.fill : str = fill
+        self.thickness : float = thickness
+    
+    def draw(self) -> int:
+        return fltk.polygone(self.points, couleur = self.colour, remplissage = self.fill, epaisseur = self.thickness)
+
 class Drawer:
 
     def __init__(self) -> None:
@@ -11,10 +22,7 @@ class Drawer:
         self.thickness : float = 2.0
 
         self.functions : List[function] = []
-        self.polygons : List[int] = []
-    
-    def add_polygon(self, points : List[Tuple[float, float]], colour : str = "black", fill : str = "") -> None:
-        self.polygons.append(fltk.polygone(points, couleur = colour, remplissage = fill, epaisseur = self.thickness))
+        self.polygons : List[Polygon] = []
 
     def run(self) -> int:
 
@@ -26,6 +34,7 @@ class Drawer:
 
             fltk.efface_tout()
 
+            for p in self.polygons: p.draw()
             for f in self.functions: f()
 
             if event_type == "Quitte": break
