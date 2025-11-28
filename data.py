c5401142337d1b1955d3eecc109f37a8e22c2bdb
theo boubel <The0messi@hotmail.com>
2025-11-28 12:30:05 +0100

    dtatata&ta

--- data.py
+++ data.py
@@ -1,13 +1,14 @@
-from shapefile import Reader
-
+import shapefile
 
 class Extractor:
     def __init__(self):
-        self.sf: Reader = Reader("fr_100km.shp")
+        self.sf: shapefile.Reader = shapefile.Reader("fr_100km.shp")
 
     def get(self, shape_id: int):
         info = {}
-        shape = self.sf.shape(shape_id)
+        shape : shapefile.Polygon = self.sf.shape(shape_id)
+        if not shape: return {}
+
         info["bbox"] = shape.bbox
         info["points"] = shape.points
         info["m"] = shape.m
