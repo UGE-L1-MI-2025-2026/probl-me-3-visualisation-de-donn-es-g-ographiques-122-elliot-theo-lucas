330a777966ad9d9523e562c85978cb01b6c3d843
ELLIOT THOMAS <elliot.thomas@nomachine.univ-eiffel.fr>
2025-12-12 11:49:52 +0100

    fix

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     idf = data_manager.get_all()
 
     for dep in idf[0]:
-        drawer.regions.append(Region(data_manager.mercarize_points(dep["points"]), data_manager.mercarize_bbox(dep["bbox"])))
+        drawer.regions.append(Region(data_manager.mercarize_points(dep["points"]), dep["parts"], data_manager.mercarize_bbox(dep["bbox"])))
 
     for crous in idf[1]:
         drawer.places.append(Place((crous["longitude"], data_manager.mercarize_int(crous["latitude"])), 5, crous))
